8999f70086483b1579775bef2eb44dda3733b9b9
bamsi <hibamsi@gmail.com>
2022-07-27 18:04:58 +0300

    update classes to fix linters

--- person.rb
+++ person.rb
@@ -1,21 +1,22 @@
 class Person
-    attr_accessor :name
-    attr_accessor :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(name = 'unkonwn', age, parent_permission: true)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'unkonwn', parent_permission: true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    private
-    def is_of_age?
-        (@age >= 18)
-    end
+  private
 
-    public
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def of_age?
+    (@age >= 18)
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require_relative 'person'
 
 class Student < Person
-    def initialize(classroom, age, name, parent_permission)
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(classroom, age, name = 'unkonwn', parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
